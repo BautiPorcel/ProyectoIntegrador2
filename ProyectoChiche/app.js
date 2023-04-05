3a255c34ba907b81d324add26fade7edb74d367e
tchaiman <tchaiman@udesa.edu.ar>
2023-04-05 18:25:00 -0300

    nose

--- ProyectoChiche/app.js
+++ ProyectoChiche/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var porbandoCosas = require("./routes/probandoIndex")
+
 var app = express();
 
 // view engine setup
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index',porbandoCosas)
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- ProyectoChiche/routes/index.js
+++ ProyectoChiche/routes/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var controller = require('/controllers/indexControllers')
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.ejs', { title: 'Express' });
-});
+router.get('/', indexControllers.home);
 
 module.exports = router;
