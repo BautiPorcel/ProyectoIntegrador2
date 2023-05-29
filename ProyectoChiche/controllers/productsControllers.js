99d3d2c7df5702495f2eca2887b282093e75425c
mpintoescalier <mpintoescalier@udesa.edu.ar>
2023-05-29 17:45:16 -0300

    Poco a poco nos acercamos

--- ProyectoChiche/controllers/productsControllers.js
+++ ProyectoChiche/controllers/productsControllers.js
@@ -1,24 +1,53 @@
-//const data = require('../data/data')
-// let db = require("../database/models/index")
-// let op = db.Sequelize.Op
-// let bcryct = requiere("bcryptjs")
+const data = require('../data/data')
+let db = require("../database/models/index")
+let op = db.Sequelize.Op
+let bcrypt = requiere("bcryptjs")
 
 const controller = {
     products: function (req,res){
         const id = req.params.id
-        res.render('product',{
-        usuarioLogueado: false,
-        productos: data.productos,
-        user: data.usuarios
+        db.productos.findByPk(id,{raw: true})
+        .then(function(data){
+            res.render('product',{
+            usuarioLogueado: false,
+            productos: data.productos,
+            user: data.usuarios
+            })
+        })
+        .catch(function(err){
+            console.log(err)
         })
     },
 
     serchResults: function (req,res){
+        let loQueEstoyBuscando = req.query.busqueda
+
+        db.productos.findAll({
+            were:{
+                nombre: {
+                    [op.like]: `%${loQueEstoyBuscando}%`
+                }
+            },
+            raw:true
+        })
+        .then(function(data){
+            let encontroResultados
+            if (data.length > 0){
+                encontroResultados = true
+            } else {
+                encontroResultados= false
+            }
+
         res.render('search-results',{
             usuarioLogueado: false,
             productos: data.productos,
             user: data.usuarios
         })
+    })
+        .catch(function(err){
+            console.log(err)
+        })
+        
     },
 
     productsAdd: function (req,res){
@@ -27,6 +56,9 @@ const controller = {
             user: data.usuarios
         })
     },
+    create:function(req,res){
+        let tituloEncriptado = bcryct
+    }
 }
 
 module.exports = controller
